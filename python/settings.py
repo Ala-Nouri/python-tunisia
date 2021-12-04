ed4031fd1d6de74061f2f97c2d3e3c20443599ee
axl1299 <alex1245nouri@gmail.com>
2021-12-04 21:19:24 +0100

    python-tunisia

--- python/settings.py
+++ python/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlewear.WhiteNoiseMiddlewear'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'python.urls'
