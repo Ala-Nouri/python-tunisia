f4ae059df63d539b4e1bc23f287bc33668a820c2
axl1299 <alex1245nouri@gmail.com>
2021-12-04 22:28:49 +0100

    python-tunisia

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,9 +3,16 @@ let togglemenu = document.querySelector('.togglemenu')
 let body = document.querySelector('body')
 let navigation =document.querySelector('.navigation')
 
+const currentTheme = localStorage.getItem("theme");
+ 
+if (currentTheme === "dark") {
+    body.classList.toggle('dark')
+    daynight.classList.toggle('active')
+  }
 daynight.onclick = function(){
 	body.classList.toggle('dark')
 	daynight.classList.toggle('active')
+	localStorage.setItem("theme", "dark");
 }
 
 togglemenu.onclick= function(){
