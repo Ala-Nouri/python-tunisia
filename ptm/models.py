6ffe844c9159317dd09d7dee16da556360699cbf
axl1299 <alex1245nouri@gmail.com>
2021-12-04 21:24:54 +0100

    python-tunisia

--- ptm/models.py
+++ ptm/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class participant (models.Model):
+    f_name = models.CharField(max_length=20)
+    l_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254,unique=True)
+    tel = models.CharField(max_length=8)
+    Choices = (
+            ('female', 'F'),
+            ('male', 'M'),
+            
+        )
+    gender = models.CharField(max_length=30, default=None, choices=Choices)
+    date = models.DateField(("date"), auto_now=False, auto_now_add=False)
+
+
+
+
+
