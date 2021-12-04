dc39f9d3032f89f0433bc3bd95c083df16444df6
axl1299 <alex1245nouri@gmail.com>
2021-12-04 23:05:38 +0100

    python-tunisia

--- ptm/admin.py
+++ ptm/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group,User
+from .models import participant
 
 # Register your models here.
+@admin.register(participant)
+class participantadmin(admin.ModelAdmin):
+    list_display = ('f_name','l_name','tel','email','gender','date')
+    list_filter = ('gender',)
+
+
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
