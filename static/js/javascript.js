d33f85d4616d8b487149a718e6514e1ecf211d26
axl1299 <alex1245nouri@gmail.com>
2021-12-06 12:03:09 +0100

    python-tunisia

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,20 +3,28 @@ let togglemenu = document.querySelector('.togglemenu')
 let body = document.querySelector('body')
 let navigation =document.querySelector('.navigation')
 
-const currentTheme = localStorage.getItem("theme");
- 
-if (currentTheme === "dark") {
-    body.classList.toggle('dark')
-    daynight.classList.toggle('active')
-  }
-daynight.onclick = function(){
-	body.classList.toggle('dark')
-	daynight.classList.toggle('active')
-	localStorage.setItem("theme", "dark");
-}
 
-togglemenu.onclick= function(){
-	togglemenu.classList.toggle('active')
-	navigation.classList.toggle('active')
-}
+function dark(){
+	if (localStorage.getItem('theme') == 'dark') {
+		body.classList.toggle('dark')
+		daynight.classList.toggle('active')
+	  }}
+	
+	
+	
+	daynight.onclick = function(){
+		body.classList.toggle('dark')
+		daynight.classList.toggle('active')
+		if (localStorage.getItem('theme') == '')
+		localStorage.setItem('theme', 'dark');
+		else
+		localStorage.setItem('theme', '');
+	}
+	
+	togglemenu.onclick= function(){
+		togglemenu.classList.toggle('active')
+		navigation.classList.toggle('active')
+	}
+	
+	window.onload=dark;
 
